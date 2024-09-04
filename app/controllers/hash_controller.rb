3a0f715d21b43ceb388f200575564f112688dc50
roderick <roderickalbertop@gmail.com>
2024-09-04 16:14:35 -0600

    sub prac5

---
+++ app/controllers/hash_controller.rb
@@ -0,0 +1,6 @@
+class HashController < ApplicationController
+  def mostrar
+    @datosHash={nombre: "Roderick", apellido: "Pérez", telefono: "87416134", correo: "rodripe@gmail.com"}
+    
+  end
+end
